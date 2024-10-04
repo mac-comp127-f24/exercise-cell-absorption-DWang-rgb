c00483d29fd492cc28e984b7a095074842ef5c71
DWang-rgb <dwang1@macalester.edu>
2024-10-04 17:05:38 -0500

    Cell Moving

--- src/cellabsorption/CellSimulation.java
+++ src/cellabsorption/CellSimulation.java
@@ -28,33 +28,42 @@ public class CellSimulation {
         populateCell();
 
         //noinspection InfiniteLoopStatement
-        while (true) {
-            Point canvasCenter = new Point(canvas.getWidth() / 2.0, canvas.getHeight() / 2.0);
-            cell.moveAround(canvasCenter);
-            cell.grow(0.02);
-
-            canvas.draw();
-            canvas.pause(10);
-        }
+        runThePoints();
+        
     }
 
     private void populateCell() {
         cells = new ArrayList<>();
         double size = rand.nextInt(5) + 2;
-        for (double i == 0; i <= 200; i ++) {}
-        
-        cell = new Cell(
-            rand.nextDouble() * (canvas.getWidth() - size),
-            rand.nextDouble() * (canvas.getWidth() - size),
-            size,
-            Color.getHSBColor(rand.nextFloat(), rand.nextFloat() * 0.5f + 0.1f, 1));
-        Ellipse theShape = cell.getShape();
-        canvas.add(theShape);
-        
+          for (int i = 0; i <= 200; i++) {
+              Cell cell = new Cell(
+              rand.nextDouble() * (canvas.getWidth() - size),
+              rand.nextDouble() * (canvas.getWidth() - size),
+              size,
+              Color.getHSBColor(rand.nextFloat(), rand.nextFloat() * 0.5f + 0.1f, 1));
+          Ellipse theShape = cell.getShape();
+          canvas.add(theShape);
+          cells.add(cell);
+      }
+      System.out.println(cells);
     }    
     
     private static double sqr(double x) {
         return x * x;
 
-    }   
+    }
+
+    private void runThePoints() {
+            for (int n = 0; n >= 0; n++) { 
+                for (Cell cell: cells) {
+                    System.out.println(cell);
+            Point canvasCenter = new Point(canvas.getWidth() / 2.0, canvas.getHeight() / 2.0);
+            cell.moveAround(canvasCenter);
+            cell.grow(0.02);
+            
+        }
+        canvas.draw();
+        canvas.pause(10);
+      }
+    }
 }
