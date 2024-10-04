03a4329a31c41f19ba55ed54d3ba6f25edf64cba
DWang-rgb <dwang1@macalester.edu>
2024-10-04 17:25:46 -0500

    Cell absorption

--- src/cellabsorption/CellSimulation.java
+++ src/cellabsorption/CellSimulation.java
@@ -59,11 +59,23 @@ public class CellSimulation {
                     System.out.println(cell);
             Point canvasCenter = new Point(canvas.getWidth() / 2.0, canvas.getHeight() / 2.0);
             cell.moveAround(canvasCenter);
-            cell.grow(0.02);
+            
             
         }
+        handleCellInteraction();
         canvas.draw();
         canvas.pause(10);
       }
     }
+
+    private void handleCellInteraction() {
+            for (int i = 0; i < cells.size(); i++) {
+                Cell cell0 = cells.get(i);
+                for (int j = i + 1; j < cells.size(); j++) {
+                    Cell cell1 = cells.get(j);
+                  // TODO: insert call here to make cell0 interact with cell1
+                  cell0.interactWith(cell1);
+              }
+          }
+      }
 }
