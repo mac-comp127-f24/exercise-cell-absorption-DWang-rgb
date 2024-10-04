e7046927a29b64ac592b42f3ea2167847e9d6f0f
DWang-rgb <dwang1@macalester.edu>
2024-10-04 15:56:55 -0500

    Extracted Cell Class-15:56

--- src/cellabsorption/CellSimulation.java
+++ src/cellabsorption/CellSimulation.java
@@ -9,16 +9,12 @@ import edu.macalester.graphics.Point;
 
 @SuppressWarnings("SameParameterValue")
 public class CellSimulation {
-    public static final double
-        WIGGLINESS = 0.2,
-        WANDER_FROM_CENTER = 60000;
+    
 
     private Cell cell;
-    private double radius;
-    private double direction;
     private CanvasWindow canvas;
     private Random rand = new Random();
-    private Ellipse shape;
+    
     
 
     public static void main(String[] args) {
@@ -42,55 +38,17 @@ public class CellSimulation {
 
     private void populateCell() {
         double size = rand.nextInt(5) + 2;
-        createCell(
+        cell = new Cell(
             rand.nextDouble() * (canvas.getWidth() - size),
             rand.nextDouble() * (canvas.getWidth() - size),
             size,
             Color.getHSBColor(rand.nextFloat(), rand.nextFloat() * 0.5f + 0.1f, 1));
-        canvas.add(shape);
-    }
-
-     private void createCell(double x, double y, double radius, Color color) {
-        shape = new Ellipse(x, y, radius * 2, radius * 2);
-        shape.setFillColor(color);
-        this.radius = radius;
-        direction = normalizeRadians(Math.random() * Math.PI * 2);
-    }
-
-    public void grow(double amount) {
-        setRadius(radius + amount);
-    }
-
-    public void setRadius(double newRadius) {
-        if (newRadius < 0) {
-            newRadius = 0;
-        }
-        radius = newRadius;
-        Point previousCenter = shape.getCenter();
-        shape.setSize(new Point(newRadius * 2, newRadius * 2));
-        shape.setCenter(previousCenter);
-    }
-
-    public void moveAround(Point centerOfGravity) {
-        shape.moveBy(Math.cos(direction), Math.sin(direction));
-
-        double distToCenter = shape.getCenter().distance(centerOfGravity);
-        double angleToCenter = centerOfGravity.subtract(shape.getCenter()).angle();
-        double turnTowardCenter = normalizeRadians(angleToCenter - direction);
-
-        direction = normalizeRadians(
-            direction
-                + (Math.random() - 0.5) * WIGGLINESS
-                + turnTowardCenter * Math.tanh(distToCenter / WANDER_FROM_CENTER));
-    }
-
-    public static double normalizeRadians(double theta) {
-        double pi2 = Math.PI * 2;
-        return ((theta + Math.PI) % pi2 + pi2) % pi2 - Math.PI;
-    }
+        Ellipse theShape = cell.getShape();
+        canvas.add(theShape);
+    }    
     
     private static double sqr(double x) {
         return x * x;
 
-    }
+    }   
 }
